be43d8ae4ff4168f4ef13962c0aea8a2983a33db
Andres Arizala <arizala13@gmail.com>
2020-11-18 14:35:37 -0600

    "Added ability to get people saved in database"

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -5,6 +5,8 @@ import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequestMapping("api/v1/person")
 
 // RestController tells it it can do get, put, post methods
@@ -23,5 +25,12 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
+    }
+
+    // define method that will serve as get request
+    // GetMapping tells it that it is a getrequest
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
     }
 }
